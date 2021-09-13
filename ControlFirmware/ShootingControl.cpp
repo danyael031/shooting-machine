d3434b27dfcb7f269c6ded728c42adcf8d0c838d
Dan Yael Sajarópulos Verdugo <danyael031@gmail.com>
2021-09-12 23:35:31 -0600

    Agragando interrupción para motor con encoder

--- ControlFirmware/ShootingControl.cpp
+++ ControlFirmware/ShootingControl.cpp
@@ -8,6 +8,8 @@ EncMotor *defaultEncMotor;
 ShootingControl::ShootingControl(){
   this->encMotor = new EncMotor(0);
   defaultEncMotor = this->encMotor;
+
+  attachInterrupt(0, [](){defaultEncMotor->handleInterrupt(); }, RISING);
 }
 
 void ShootingControl::setup(){
